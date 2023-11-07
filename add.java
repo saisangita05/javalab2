ed900e2127dd11be20f185593787427bdf638e3b
Sai sangita Adhek <130208503+saisangita05@users.noreply.github.com>
2023-11-07 12:54:38 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+import javax.sound.midi.Soundbank;
+class add {
+    public static void main(String args[])
+    {
+        /*int sum,a,b,diff,mul,div;
+        Scanner s=new Scanner(System.in);
+        System.out.println("enter two numbers:");
+        a=s.nextInt();
+        b=s.nextInt();
+        sum=a+b;
+        diff=a-b;
+        mul=a*b;
+        div=a/b;
+        System.out.println("the sum is"+sum);
+        System.out.println("the diff is"+diff);
+        System.out.println("the mul is"+mul);
+        System.out.println("the div is"+div);*/
+
+
+        
+        /*literals of numerical datatypes */
+        int a=100101001;
+        System.out.println(a);
+        int b=0B100;
+         System.out.println(b);
+         int c=071;
+         System.out.println(c);
+         int d=0X7f;
+          System.out.println(d);
+
+
+
+    }
+    
+}
